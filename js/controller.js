2db06bd49e40429ce0bca2fc6321be00da76c1b3
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-17 22:54:03 +0000

    Seperated the javascript files in a different folder

---
+++ js/controller.js
@@ -0,0 +1,43 @@
+<script type="text/javascript">
+	var computerChoice = Math.random();
+	if (computerChoice < 0.33) {
+    	computerChoice = "rock";
+	} else if(computerChoice <= 0.66) {
+    	computerChoice = "paper";
+	} else {
+    	computerChoice = "scissors";
+	}
+	document.getElementById("box1").onclick= function(){
+		if(computerChoice == "rock"){
+			alert("It is a tie, You chose Rock, computer chose Rock, Lame!");
+		}
+		else if(computerChoice =="paper"){
+			alert("Sucker, YOU LOST! You chose Rock, COMPUTER OVERLORD chose paper");
+		}
+		else if(computerChoice=="scissors"){
+			alert("DANG! You Beat Computer OVERLORD cuz he chose scissors");
+		}
+	}
+	document.getElementById("box2").onclick= function(){
+		if(computerChoice == "paper"){
+			alert("It is a tie, You chose Paper, computer chose Paper, Lame!");
+		}
+		else if(computerChoice =="scissors"){
+			alert("Sucker, YOU LOST! You chose Paper, COMPUTER OVERLORD chose scissors");
+		}
+		else if(computerChoice=="rock"){
+			alert("DANG! You Beat Computer OVERLORD cuz he chose rock");
+		}
+	}
+	document.getElementById("box3").onclick= function(){
+		if(computerChoice == "scissors"){
+			alert("It is a tie, You chose scissors, computer chose scissors, Lame!");
+		}
+		else if(computerChoice =="rock"){
+			alert("Sucker, YOU LOST! You chose scissors, COMPUTER OVERLORD chose rock");
+		}
+		else if(computerChoice=="paper"){
+			alert("DANG! You Beat Computer OVERLORD cuz he chose paper");
+		}
+	}
+</script>
